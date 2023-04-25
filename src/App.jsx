63b29c844bc53d28dd820c01ba1454475f3ae5c4
Amandalfs <caduc57@gmail.com>
2023-04-25 00:29:51 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import'./componetes/AssinaturaNewsletter/EstiloAssinaturaNewsletter.css';
 import AssinaturaNewsletter from './componetes/AssinaturaNewsletter/AssinaturaNewsletter.jsx';
-import Infos from './componetes/infos/Infos.jsx';
+
 import './componetes/infos/EstiloInfos.css'
+import Infos from './componetes/infos/Infos.jsx';
 
 
 function App() {
