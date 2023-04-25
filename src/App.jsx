11485cc55d7a73d1acbc95d24cc85a8cc89aaa11
Amandalfs <caduc57@gmail.com>
2023-04-25 00:13:06 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import'./componetes/AssinaturaNewsletter/EstiloAssinaturaNewsletter.css';
 import AssinaturaNewsletter from './componetes/AssinaturaNewsletter/AssinaturaNewsletter.jsx';
-import Infos from './componetes/infos/infos';
+import Infos from './componetes/infos/Infos';
 import './componetes/infos/EstiloInfos.css'
 
 
